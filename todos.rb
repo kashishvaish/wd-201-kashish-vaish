6b4e52d2067b4ddc924154a3df6d92d553c3c652
Kashish Vaish <kashish.vaish.cs.2018@miet.ac.in>
2021-05-23 13:37:50 +0530

    My first todo program

---
+++ todos.rb
@@ -0,0 +1,9 @@
+#ask for a list of todos
+#and print them back to us
+
+puts "What is your next todo?"
+next_todo = gets
+
+puts "\n\n\n" #Carriage return
+
+puts "[ ] #{next_todo}" #String Interpolation
